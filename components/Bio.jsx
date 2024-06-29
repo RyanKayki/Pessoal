834b77bb8c0d24dbc2a206fdedcf5fc122175429
RyanKayki <129207965+RyanKayki@users.noreply.github.com>
2024-06-29 20:22:58 -0300

    First Commit

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -14,7 +14,7 @@ const Bio = () => {
         <img src="eu/amigos.jpg" alt="Ryan Kayki e amigos" />
         <div className="more">
           <p><strong>Olá! Eu sou Ryan Kayki e esses são meus amigos, sou um estudante e tenho 17 anos, sou apaixonado por Desenvolvimento de Sistemas, especializado em desenvolvimento full-stack. Atualmente, meu foco está no back-end, onde passo a maior parte do meu tempo explorando e aprendendo frameworks na linguagem Python, que é minha verdadeira paixão.</strong></p>
-          <p><strong>Abaixo vou deixar alguns dos meus projetos que fiz e minhas redes sociais caso queiram trocar uma ideia. Obrigado</strong></p>
+          <p><strong>Abaixo vou deixar alguns dos meus projetos que fiz e minhas redes sociais caso queiram trocar uma ideia. Obrigado.</strong></p>
         </div>
       </div>
     </section>
