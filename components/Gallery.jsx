09c8a8d06bcf23b4b32b45875027433e41e74b8c
RyanKayki <129207965+RyanKayki@users.noreply.github.com>
2025-02-11 03:14:10 -0300

    tirando link

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -17,9 +17,7 @@ const Gallery = () => {
         <a href="https://ryankayki.github.io/reclame-aqui/" target="_blank" rel="noopener noreferrer">
           <img src="projetos/reclame.png" alt="Suporte" />
         </a>
-        <a href="" target="_blank" rel="noopener noreferrer">
           <img src="projetos/tec.png" alt="Tio Carlos Concerta" />
-        </a>
       </div>
     </section>
   );
